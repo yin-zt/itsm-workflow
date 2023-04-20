ddc67a4347360dc9a1d4d647d994bd381785c8a1
mailongyin <mailongyin@lexin.com>
2023-04-20 10:55:30 +0800

    增加跨域中间件

--- pkg/routes/a_routes.go
+++ pkg/routes/a_routes.go
@@ -4,13 +4,14 @@ import (
 	log "github.com/cihub/seelog"
 	"github.com/gin-gonic/gin"
 	"github.com/yin-zt/itsm-workflow/pkg/config"
+	"github.com/yin-zt/itsm-workflow/pkg/middleware"
 )
 
 // 初始化
 func InitRoutes() *gin.Engine {
 	defer log.Flush()
 	//设置模式
-	//gin.SetMode(config.Conf.System.Mode)
+	gin.SetMode(config.Mode)
 
 	// 创建带有默认中间件的路由:
 	// 日志与恢复中间件
@@ -25,12 +26,19 @@ func InitRoutes() *gin.Engine {
 	// 启用操作日志中间件
 	//r.Use(middleware.OperationLogMiddleware())
 
+	// 启用全局跨域中间件
+	r.Use(middleware.CORSMiddleware())
+
+	// 初始化JWT认证中间件
+	authMiddleware, err := middleware.InitAuth()
+
 	// 路由分组
 	apiGroup := r.Group("/" + config.UrlPathPrefix)
 
 	// 注册路由
 	InitBaseRoutes(apiGroup)  // 注册基础路由
 	InitOrderRoutes(apiGroup) // 注册工单路由
+	InitCmdbRoutes(apiGroup)  // 注册CMDB路由
 
 	log.Info("初始化路由完成！")
 	return r
